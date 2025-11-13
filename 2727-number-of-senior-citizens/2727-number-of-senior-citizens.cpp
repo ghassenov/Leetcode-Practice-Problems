ef0a077469541d29ecc4ba2d6850e5ce497a7042
Ghassen Naouar <ghassennaouar7@gmail.com>
2025-11-13 22:45:03 +0100

    Time: 0 ms (100%), Space: 17.3 MB (43.27%) - LeetHub

---
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count =0;
+        int n = details.size();
+        for(auto x:details){
+            if((x[11]-'0')*10+(x[12]-'0')>60) count++;
+        }
+        return count;
+    }
+};
