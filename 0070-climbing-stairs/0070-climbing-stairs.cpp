c1aea800bf912d690a6acb9c53bc67589103be4b
Ghassen Naouar <ghassennaouar7@gmail.com>
2026-03-19 16:35:49 +0100

    0070-CLIMBING-STAIRS Stats: Time: 0 ms (100%), Space: 8.5 MB (46.74%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,12 @@
 class Solution {
-
-private:
-    int f(int idx,vector<int> &dp){
-        if(idx == 0) return 1;
-        if(idx < 0) return 0;
-
-        if(dp[idx] != -1) return dp[idx];
-
-        //case 1-step
-        int left = f(idx-1,dp);
-        int right = f(idx-2,dp);
-
-        return dp[idx] = left + right;
-    }
 public:
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        return f(n,dp);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
