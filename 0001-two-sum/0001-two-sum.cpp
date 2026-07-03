8c3d5552314d2a64c142d2984da46790fd785277
Ghassen Naouar <ghassennaouar7@gmail.com>
2026-07-03 20:48:46 +0100

    0001-TWO-SUM Stats: Time: 3 ms (66.86%), Space: 14.8 MB (54.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,38 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        multimap<int, int> mp;
-        vector<int> v;
+        unordered_map<int,int> mp;
         int n = nums.size();
-        
-        for(int i = 0; i < n; i++) {
-            mp.insert({nums[i], i});
-        }
-        
-        sort(nums.begin(), nums.end());
-        int l = 0;
-        int r = n - 1;
-        
-        while(l < r) {
-            int s = nums[l] + nums[r];
-            if(s == target) {
-                auto it1 = mp.find(nums[l]);
-                int idx1 = it1->second;
-                mp.erase(it1); 
-                
-                auto it2 = mp.find(nums[r]);
-                int idx2 = it2->second;
-                
-                v.push_back(idx1);
-                v.push_back(idx2);
-                return v;
+        for(int i=0;i<n;i++){
+            int complement = target - nums[i];
+            if(mp.count(complement)){
+                return {mp[complement],i};
             }
-            else if(s < target) {
-                l++;
-            }
-            else {
-                r--;
-            }
+            mp[nums[i]] = i;
         }
         return {};
     }
