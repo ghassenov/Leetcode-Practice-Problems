d394674c3ea95da00a7848bfaaf49136e1ad935d
Ghassen Naouar <ghassennaouar7@gmail.com>
2026-07-03 19:49:59 +0100

    0217-CONTAINS-DUPLICATE Stats: Time: 18 ms (96.31%), Space: 82.9 MB (78.11%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         int n = nums.size();
-        // first we can sort the array
         sort(nums.begin(),nums.end());
-        // then we check for adjacent elements
+        bool ans = false;
         for(int i=0;i<n-1;i++){
             if(nums[i] == nums[i+1]){
-                return true;
+                ans = true;
+                break;
             }
         }
-        return false;
+        return ans;
     }
 };
