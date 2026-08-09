88165c8f3204866ce7ba32f62e5b46f4ecae62a4
Ghassen Naouar <ghassennaouar7@gmail.com>
2026-08-09 23:55:34 +0100

    0283-MOVE-ZEROES Stats: Time: 43 ms (6.07%), Space: 23.9 MB (19.88%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,23 +2,19 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        int p1= 0;
-        int p2 =1;
-        while(p2<n){
-            if(nums[p1] == 0 && nums[p2]!= 0){
-                int temp = nums[p1];
-                nums[p1] = nums[p2];
-                nums[p2] = temp;
-                p1++;
-                p2++;
-            }
-            else if(nums[p1] !=0){
-                p1++;
-                p2++;
-            }
-            else{
-                p2++;
+        for(int i=0;i<n;i++){
+            if(nums[i] == 0){
+                int j = i+1;
+                while(j<n){
+                    if(nums[j] != 0){
+                        swap(nums[i],nums[j]);
+                        break;
+                    }
+                    j++;
+                }
             }
         }
+        // 0 1 0 3 12
+        // 
     }
 };
