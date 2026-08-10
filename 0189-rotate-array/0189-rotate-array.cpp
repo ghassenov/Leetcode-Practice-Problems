9679105c429c4dfd443f4f3632e07857bed15d95
Ghassen Naouar <ghassennaouar7@gmail.com>
2026-08-10 22:58:39 +0100

    0189-ROTATE-ARRAY Stats: Time: 0 ms (100%), Space: 29.6 MB (70.55%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
+    void reverse(vector<int> &nums,int start,int end){
+        while(start < end){
+            swap(nums[start],nums[end]);
+            start++;
+            end--;
+        }
+    }
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> temp(n,0);
-        
-        for(int i=0;i<n;i++){
-            temp[(i+k)%n] = nums[i];
-        }
-        for(int i=0;i<n;i++){
-            nums[i] = temp[i];
-        }
+        k = k%n;
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
     }
 };
