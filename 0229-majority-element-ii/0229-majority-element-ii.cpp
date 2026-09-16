093eb5c2efd8a21adaca4a9fd12d780bdb6c50b3
Ghassen Naouar <ghassennaouar7@gmail.com>
2026-09-16 22:11:09 +0100

    0229-MAJORITY-ELEMENT-II Stats: Time: 11 ms (19.33%), Space: 27.3 MB (5.68%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ans;
+        int n = nums.size();
+        map<int,int> mp;
+        for(auto x:nums){
+            mp[x]++;
+        }
+        for(auto x:mp){
+            if(x.second >n/3) ans.push_back(x.first);
+        }
+        return ans;
+    }
+};
