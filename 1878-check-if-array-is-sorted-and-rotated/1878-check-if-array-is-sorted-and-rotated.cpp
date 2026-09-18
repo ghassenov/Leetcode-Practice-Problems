0ffd95a69a553de570980ae85014ef96f542abc1
Ghassen Naouar <ghassennaouar7@gmail.com>
2026-09-18 21:02:01 +0100

    1878-CHECK-IF-ARRAY-IS-SORTED-AND-ROTATED Stats: Time: 0 ms (100%), Space: 11.2 MB (56.05%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        // we can calculate the number of inversion (i<j && arr[i]>arr[j]) (don't miss arr[0] && arr [n-1] if exists an inversion)
-        // if n = 0 then we have the original array,
-        // if n = 1 return true
-        // else return false
+        // we can count the number of infliction points
+        // if number equals 0 or 1 then true
+        // if infliction point >1 then no
         int n = nums.size();
-        int n_inversion = 0;
+        int n_inflictions = 0;
         for(int i=0;i<n-1;i++){
-            if(nums[i]>nums[i+1]) n_inversion++;
+            if(nums[i]>nums[i+1]) n_inflictions++;
         }
-        if(n_inversion == 0) return true;
-        else if(n_inversion == 1){
-            if(nums[0] < nums[n-1]) return false;
-            else return true;
-        }
-        else return false;
+        if(nums[0]<nums[n-1]) n_inflictions++;
+        if(n_inflictions > 1) return false;
+        else return true;
     }
 };
