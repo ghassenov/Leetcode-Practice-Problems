5f460d2a553f5e0b52d575b93d81199eb01157f5
Ghassen Naouar <ghassennaouar7@gmail.com>
2026-09-19 22:06:35 +0100

    0075-SORT-COLORS Stats: Time: 0 ms (100%), Space: 11.7 MB (11.4%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int red = 0;
-        int white = 0;
-        int blue = 0;
         int n = nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i] == 0)red++;
-            else if(nums[i] == 1)white++;
-            else blue++;
+        map<int,int> mp;
+        for(auto x:nums){
+            mp[x]++;
         }
-        int k = 0;
-        while(red--){
-            nums[k] = 0;
-            k++;
-        }
-        while(white--){
-            nums[k] = 1;
-            k++;
-        }
-        while(blue--){
-            nums[k] = 2;
-            k++;
+        int i=0;
+        for(auto x:mp){
+            for(int j=0;j<x.second;j++){
+                nums[i] = x.first;
+                i++;
+            }
         }
     }
 };
