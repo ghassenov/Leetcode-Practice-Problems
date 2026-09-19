e8904904c53e0895dd62c51c4d7126c516f07b19
Ghassen Naouar <ghassennaouar7@gmail.com>
2026-09-19 23:42:43 +0100

    0169-MAJORITY-ELEMENT Stats: Time: 5 ms (14%), Space: 42.1 MB (5.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // majority element is the element appearing more than [n/2]
-        int ans = -1;
         int n = nums.size();
-        map<int,int> mp;
+        unordered_map<int,int> mp;
         for(auto x:nums){
             mp[x]++;
-            if(mp[x] > n/2) ans = x;
+            if(mp[x]> n/2){
+                return x;
+            }
         }
-        return ans;
+        return -1;
     }
 };
