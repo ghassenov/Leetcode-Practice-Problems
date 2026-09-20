70a2a53ab5cfc54c479a9f22b0853ec896dfd533
Ghassen Naouar <ghassennaouar7@gmail.com>
2026-09-20 20:58:18 +0100

    0121-BEST-TIME-TO-BUY-AND-SELL-STOCK Stats: Time: 0 ms (100%), Space: 97.2 MB (88.14%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,20 +3,18 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         int ans = 0;
-
+        if(n == 1) return 0;
         int l = 0;
         int r = 1;
         while(r<n){
             if(prices[l] > prices[r]){
                 l = r;
-                r++;
             }
             else{
-                ans = max(ans,prices[r] - prices[l]);
-                r++;
+                ans = max(ans,prices[r]-prices[l]);
             }
+            r++;
         }
         return ans;
-
     }
 };
